9817206e88f50dcc8a406ffdee058b6db09f5d72
adenuga adewumi efe <79708712+Brain-codes@users.noreply.github.com>
2023-06-13 22:53:05 +0100

    nothing serious

--- lib/shared/components/snackbar/snack_bar.dart
+++ lib/shared/components/snackbar/snack_bar.dart
@@ -4,26 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void successSnackBar(String title, String message) {
-  Get.snackbar(title, message,
-      colorText: Colors.white,
-      duration: const Duration(seconds: 3),
-      snackPosition: SnackPosition.TOP,
-      backgroundColor: Colors.green,
-      icon: Icon(
-        Icons.check_circle_sharp,
-        color: Colors.white,
-      ));
+  Get.snackbar(
+    title,
+    message,
+    colorText: Colors.white,
+    duration: const Duration(seconds: 3),
+    snackPosition: SnackPosition.TOP,
+    backgroundColor: Colors.green,
+    icon: Icon(
+      Icons.check_circle_sharp,
+      color: Colors.white,
+    ),
+  );
 }
 
 void errorSnackBar(String title, String message) {
-  Get.snackbar(title, message,
-      colorText: Colors.white,
-      duration: const Duration(seconds: 3),
-      snackPosition: SnackPosition.TOP,
-      backgroundColor: Colors.red,
-      icon: Icon(
-        Icons.error_outline,
-        color: Colors.white,
-      ));
+  Get.snackbar(
+    title,
+    message,
+    colorText: Colors.white,
+    duration: const Duration(seconds: 3),
+    snackPosition: SnackPosition.TOP,
+    backgroundColor: Colors.red,
+    icon: Icon(
+      Icons.error_outline,
+      color: Colors.white,
+    ),
+  );
 }
-
